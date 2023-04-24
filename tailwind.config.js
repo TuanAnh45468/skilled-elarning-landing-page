0913a2f562b69d50e455000f797abb42b92a82b0
tuana <tuananh45468@gmail.com>
2023-04-24 13:51:30 +0700

    complete features section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
   theme: {
     colors: {
       "slate-blue": "#666CA3",
+      white: "hsl(0, 0%, 100%)",
       "dark-blue": "#13183F",
       "cool-gray": "#83869A",
       "deep-pink": "#F74780",
@@ -21,6 +22,7 @@ module.exports = {
       "body-s": ["16px", { lineHeight: "28px", fontWeight: 700 }],
       "body-m": ["18px", { lineHeight: "28px", fontWeight: 500 }],
       "heading-s": ["24px", { lineHeight: "28px", fontWeight: 800 }],
+      "2s": ["28px"],
       "heading-m": ["32px", { lineHeight: "40px", fontWeight: 800 }],
       "heading-l": ["40px", { lineHeight: "51px", fontWeight: 800 }],
       "heading-xl": ["56px", { lineHeight: "70px", fontWeight: 800 }],
@@ -30,6 +32,8 @@ module.exports = {
       backgroundImage: {
         "gradient-pink-orange":
           "linear-gradient(to bottom, #FF6F48 0%, #F02AA6 100%)",
+        "gradient-pink-orange-pale":
+          "linear-gradient(to bottom, #FFB6A3 0%, #F78FB3 100%)",
         "gradient-blue-pink":
           "linear-gradient(to bottom, #4851FF 0%, #F02AA6 100%)",
       },
